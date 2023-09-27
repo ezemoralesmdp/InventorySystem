a080beb262b76c014d0f2dcae8e0c243ca3c5287
Ezequiel <emorales@emmsa.net>
2023-09-27 16:24:23 -0300

    :email: Email

--- InventorySystem.Utils/EmailSender.cs
+++ InventorySystem.Utils/EmailSender.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace InventorySystem.Utils
 {
     public class EmailSender : IEmailSender
     {
+        public string SendGridSecret { get; set; }
+
+        public EmailSender(IConfiguration _config)
+        {
+            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey"); ;
+        }
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            var client = new SendGridClient(SendGridSecret);
+            var from = new EmailAddress("emorales@emmsa.net");
+            var to = new EmailAddress(email);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
+            return client.SendEmailAsync(msg);
         }
     }
 }
